80efb9c004c4beaa2b16a8d4abbff0211e744134
ZlataVinokur <sasha-popov04@list.ru>
2024-02-29 12:03:38 +0300

    Script exit +folders

---
+++ Assets/Scripts/Buttons mechanics/Buttons.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    private void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+
+
+}
