462c9638f382bc1e68bfc96c51f81a909c59cd88
ZlataVinokur <vadimpavlov@bk.ru>
2024-04-18 12:07:16 +0300

    Skills

--- Assets/Scripts/GestureEvents.cs
+++ Assets/Scripts/GestureEvents.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GestureEvents : MonoBehaviour
 {
+    GestureRecognition gesture = new GestureRecognition();
+
+    private void Start()
+    {
+        //gesture.contdIdentify(GestureRecognition.UpdateHeadPositionPolicy.UseInitial, GestureRecognition.UpdateHeadPositionPolicy.UseInitial, ref );
+    }
+
+    private void Update()
+    {
+        
+    }
+
     public void OnGestureCompleted(GestureCompletionData gestureCompletionData)
     {
+
         if (gestureCompletionData.gestureID < 0) 
         {
             Debug.Log("LOX");
             return;
         }
-        if (gestureCompletionData.similarity >= 0.5)
+
+        if (gestureCompletionData.gestureName == "vlad")
         {
-            Debug.Log("gg");
+            Debug.Log("vlad");
         }
+
+        if (gestureCompletionData.gestureName == "oleg")
+        {
+            Debug.Log("oleg");
+        }
+
+        
     }
 }
