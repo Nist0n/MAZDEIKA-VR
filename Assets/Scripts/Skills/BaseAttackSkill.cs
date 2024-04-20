2ee26072e8862a828ad9a1d319bad4e5abd2fde4
Niston <vadimpavlov@bk.ru>
2024-04-21 00:25:57 +0300

    BaseAttackSkill

---
+++ Assets/Scripts/Skills/BaseAttackSkill.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAttackSkill : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject hit;
+
+    private bool isAttacking = false;
+    
+    private GameObject _enemy;
+    void Start()
+    {
+        _enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _enemy.transform.position, speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+        hit.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Skills/Skills.cs
+++ Assets/Scripts/Skills/Skills.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Skills : MonoBehaviour
 {
+    [SerializeField] private GameObject baseAttackSpell;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            BaseAttack();
+        }
+    }
+
     public void BaseAttack()
     {
-
+        Instantiate(baseAttackSpell, transform);
     }
 
 }
