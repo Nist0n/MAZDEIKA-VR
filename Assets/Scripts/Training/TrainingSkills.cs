5a5559547e8cc07365778e5489db796fdb6a5045
Niston <vadimpavlov@bk.ru>
2024-05-10 02:53:42 +0300

    training

---
+++ Assets/Scripts/Training/TrainingSkills.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingSkills : MonoBehaviour
+{
+    [SerializeField] private GameObject baseAttackSpell;
+    [SerializeField] private GameObject breakShieldSkill;
+
+    private PlayerController _player;
+    private FirstEnemy _enemy;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+        _enemy = FindObjectOfType<FirstEnemy>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            BaseAttack();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            BreakShield();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ActivateShield();
+        }
+    }
+
+    public void BaseAttack()
+    {
+        Instantiate(baseAttackSpell, transform);
+    }
+
+    public void BreakShield()
+    {
+        Instantiate(breakShieldSkill, transform);
+    }
+
+    public void ActivateShield()
+    {
+        StartCoroutine(_player.ActivateShield());
+    }
+}
