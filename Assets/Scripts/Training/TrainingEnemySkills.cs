8bfdaedf2acc15a4a33057fbefb5bddb6b7cc410
Niston <vadimpavlov@bk.ru>
2024-05-17 22:45:27 +0300

    SecondSkillTraining

---
+++ Assets/Scripts/Training/TrainingEnemySkills.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainingEnemySkills : MonoBehaviour
+{
+    [SerializeField] private GameObject baseAttackSkill;
+    [SerializeField] private Image skillImage;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject hand;
+
+    private FirstEnemy _enemy;
+    private PlayerController _player;
+
+    private bool _isAttacking = false;
+    private float _time;
+    private float _timer;
+    private int _randomNumOfSkill;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+        _enemy = FindObjectOfType<FirstEnemy>();
+        _randomNumOfSkill = 0;
+        _time = Random.Range(2, 4);
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_time - _timer <= 1f && !_isAttacking)
+        {
+            _isAttacking = true;
+            CastSkill();
+        }
+    }
+
+    private void CastSkill()
+    {
+        List<IEnumerator> functions = new List<IEnumerator>();
+        functions.Add(BaseAttack());
+        StartCoroutine(functions[_randomNumOfSkill]);
+        if (functions[_randomNumOfSkill].ToString().Contains("BaseAttack")) Debug.Log("BaseAttack");
+        _randomNumOfSkill = Random.Range(0, functions.Count);
+    }
+
+    IEnumerator BaseAttack()
+    {
+        animator.SetTrigger("baseAttack");
+        skillImage.sprite = baseAttackSkill.GetComponent<Image>().sprite;
+        skillImage.color = new Color(255f, 255f, 255f, 255f);
+        yield return new WaitForSeconds(1f);
+        Instantiate(baseAttackSkill, hand.transform.position, Quaternion.identity, _enemy.transform);
+        _timer = 0;
+        _time = Random.Range(3, 4);
+        _isAttacking = false;
+        skillImage.color = new Color(255f, 255f, 255f, 0f);
+    }
+}
--- Assets/Scripts/Training/TrainingManager.cs
+++ Assets/Scripts/Training/TrainingManager.cs
@@ -9,6 +9,7 @@ public class TrainingManager : MonoBehaviour
 {
     [SerializeField] private NPCConversation startingDialog;
     [SerializeField] private NPCConversation secondDialog;
+    [SerializeField] private NPCConversation thirdDialog;
     [SerializeField] private TextMeshProUGUI baseAttackTrainingText;
     [SerializeField] private Animator textAnim;
     [SerializeField] private Animator baseAttackVideoAnimator;
@@ -19,18 +20,25 @@ public class TrainingManager : MonoBehaviour
     public bool SecondSkillTraining = false;
     public bool ThirdSkillTraining = false;
     public int BaseAttackCompletedTimes;
+    public int ShieldCompletedTimes;
 
     private bool _training1Completed = false;
+    private bool _training2Completed = false;
+
+    private TrainingEnemySkills _trainingEnemySkills;
 
     private void Start()
     {
+        _trainingEnemySkills = FindObjectOfType<TrainingEnemySkills>();
         StartDialog(startingDialog);
     }
 
     private void Update()
     {
         BaseAttackCompletedTimes = Mathf.Clamp(BaseAttackCompletedTimes, 0, 5);
+        ShieldCompletedTimes = Mathf.Clamp(ShieldCompletedTimes, 0, 5);
         if (!_training1Completed) CheckProgressCompletionBaseAttackTraining();
+        if (!_training2Completed && _training1Completed) CheckProgressCompletionShieldTraining();
     }
 
     private void StartDialog(NPCConversation dialog)
@@ -51,10 +59,32 @@ public class TrainingManager : MonoBehaviour
             StartDialog(secondDialog);
         }
     }
+    
+    private void CheckProgressCompletionShieldTraining()
+    {
+        baseAttackTrainingText.color = Color.red;
+        baseAttackTrainingText.text = $"Успешно отражено атак: {ShieldCompletedTimes}/5";
+        if (ShieldCompletedTimes >= 5)
+        {
+            baseAttackTrainingText.color = Color.green;
+            _training2Completed = true;
+            textAnim.SetTrigger("hideText");
+            shieldVideoAnimator.SetTrigger("hideVideo");
+            _trainingEnemySkills.enabled = false;
+            StartDialog(thirdDialog);
+        }
+    }
 
     public void StartTraining1()
     {
         baseAttackTrainingText.gameObject.SetActive(true);
         FirstSkillTraining = true;
+    }
+    
+    public void StartTraining2()
+    {
+        baseAttackTrainingText.gameObject.SetActive(true);
+        SecondSkillTraining = true;
+        _trainingEnemySkills.enabled = true;
     }
 }
